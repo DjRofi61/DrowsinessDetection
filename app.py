577088aa90326a50e21eed846abd91cb1a82073e
$get_rofi_by1 <r.djaber@esi-sba.dz>
2024-05-30 09:03:27 +0100

    delete docker file content

--- app.py
+++ app.py
@@ -116,17 +116,9 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-# Set the SDL_AUDIODRIVER to 'dummy' if no audio device is present
-os.environ["SDL_AUDIODRIVER"] = "dummy"
+
 
 pygame.mixer.init()
-
-# Try to initialize the mixer
-try:
-    pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
-    print("Pygame mixer initialized successfully.")
-except pygame.error as e:
-    print(f"Failed to initialize Pygame mixer: {e}")
 
 def play_alert_sound():
     pygame.mixer.music.load("alert_sound.mp3")  # Replace "alert_sound.mp3" with your sound file
