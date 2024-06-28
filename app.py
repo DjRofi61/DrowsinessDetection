5928dc67b9d6ab4f89bc066080bf8e34e5bc8e70
$get_rofi_by1 <r.djaber@esi-sba.dz>
2024-06-29 00:11:46 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import bcrypt
+from flask import Flask, render_template, Response, request, redirect, url_for
+from PIL import Image
+import cv2
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+import pygame
+from flask_bcrypt import Bcrypt
+from ultralytics import YOLO
+import os
+import numpy as np  # Add this import
 from flask import Flask, render_template, Response, request
 from PIL import Image
 import cv2
+import torch.nn.functional as F  # Add this import
 import torch
 import torch.nn as nn
 from torchvision import models, transforms
@@ -14,7 +26,269 @@ from flask_login import UserMixin, login_user, current_user, logout_user, login_
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from ultralytics import YOLO  # Add this import
+import os
+import psycopg2
 
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+app.config['RESULT_FOLDER'] = 'static/results/'
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+app.config['SECRET_KEY'] = os.urandom(24)
+mydb = psycopg2.connect(database="driverdrowsinessdetection",
+                        user="driverdrowsinessdetection_user",
+                        password="UAbXYKVabEm2uZjC0KR6P54YsUX1z0qU",
+                        host="dpg-cpvj2quehbks73duqkcg-a.oregon-postgres.render.com", port="5432")
+
+# Create cursor object
+
+mycursor = mydb.cursor()
+
+class User(UserMixin):
+    def __init__(self, id, username, email, password):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
+
+    @staticmethod
+    def get(user_id):
+        query = "SELECT * FROM users WHERE id = %s"
+        mycursor.execute(query, (user_id,))
+        user = mycursor.fetchone()
+        if user:
+            return User(user[0], user[1], user[2], user[3])
+        return None
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = StringField('password', validators=[DataRequired()])
+    confirm_password = StringField('confirm_password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        # Query to check if the username exists
+        query = "SELECT username FROM users WHERE username = %s"
+        mycursor.execute(query, (username.data,))
+        user = mycursor.fetchone()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        # Query to check if the email exists
+        query = "SELECT email FROM users WHERE email = %s"
+        mycursor.execute(query, (email.data,))
+        user = mycursor.fetchone()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+
+        query = "INSERT INTO users (username, email, password) VALUES (%s, %s, %s)"
+        mycursor.execute(query, (form.username.data, form.email.data, form.password.data))
+        mydb.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for(''))
+    form = LoginForm()
+    if form.validate_on_submit():
+        query = "SELECT * FROM users WHERE email = %s"
+        mycursor.execute(query, (form.email.data,))
+        user = mycursor.fetchone()
+        print(user)
+        #print(user[0])
+        if user and user[2] == form.password.data :
+            print('You are logged in')
+            return render_template('index.html ' , )
+        else:
+            print('Login Unsuccessful. Please check email and password')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+pygame.mixer.init()
+
+def play_alert_sound():
+    pygame.mixer.music.load("alert_sound.mp3")  # Replace "alert_sound.mp3" with your sound file
+    pygame.mixer.music.play()
+
+def stop_alert():
+    pygame.mixer.music.stop()  # Assuming you're using pygame.mixer.music for playing the alert sound
+
+# Define the model
+class CustomModel(nn.Module):
+    def __init__(self, num_classes=3):
+        super(CustomModel, self).__init__()
+        self.mobilenet_v2 = models.mobilenet_v2(pretrained=True)
+        self.mobilenet_v2.classifier = nn.Identity()
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(0.3)
+        self.fc1 = nn.Linear(1280, 8)
+        self.fc2 = nn.Linear(8, num_classes)
+
+    def forward(self, x):
+        x = self.mobilenet_v2(x)
+        x = self.flatten(x)
+        x = self.dropout(x)
+        x = torch.relu(self.fc1(x))
+        x = torch.softmax(self.fc2(x), dim=1)
+        return x
+
+# Instantiate the model
+model = CustomModel(num_classes=3)
+model.load_state_dict(torch.load('b.pth'), strict=False)
+model.eval()
+
+# Load YOLO model for face detection
+yolo_model = YOLO("yolov8-face/yolov8n-face.pt")
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+
+
+@app.route('/predict_predefined', methods=['POST'])
+def predict_predefined():
+    image_path = request.form.get('image_path')
+    if image_path:
+        # Perform prediction
+        result_filepath = predict_drowsiness(image_path)
+
+        # Extract filename from path
+        filename = os.path.basename(image_path)
+
+        return render_template('predict.html', filename=filename)
+    return redirect(url_for('index'))
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'image' not in request.files:
+        return redirect(request.url)
+    file = request.files['image']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = file.filename
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+
+        # Perform prediction
+        result_filepath = predict_drowsiness(filepath)
+
+        return render_template('predict.html', filename=filename)
+
+def predict_drowsiness(image_path):
+    alert_triggered = False
+    img = Image.open(image_path).convert('RGB')
+    frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+
+    # Detect faces in the frame using YOLO
+    results = yolo_model(frame)
+
+    for result in results:
+        for bbox in result.boxes:
+            x1, y1, x2, y2 = map(int, bbox.xyxy[0])
+
+            # Crop the face
+            face_img = frame[y1:y2, x1:x2]
+
+            # Convert the face image from NumPy array to PIL image
+            face_pil = Image.fromarray(cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB))
+
+            # Pre-process the face for model input
+            face_tensor = transform(face_pil).unsqueeze(0)  # Add batch dimension
+
+            # Make prediction
+            with torch.no_grad():
+                output = model(face_tensor)
+                _, predicted_class = torch.max(output, 1)
+
+            # Convert prediction to string
+            class_names = ['yawning', 'closeEyes', 'awake']
+            prediction_text = class_names[predicted_class.item()]
+
+            if prediction_text in ['closeEyes', 'yawning']:
+                if not alert_triggered:
+                    play_alert_sound()
+                    alert_triggered = True
+            else:
+                if alert_triggered:
+                    stop_alert()
+                    alert_triggered = False
+
+            # Draw rectangle around the face
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+            # Write prediction result on the frame
+            cv2.putText(frame, prediction_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+
+    # Save the result image
+    result_filename = os.path.basename(image_path)
+    result_filepath = os.path.join(app.config['RESULT_FOLDER'], result_filename)
+    cv2.imwrite(result_filepath, frame)
+
+    return result_filepath
+
+if __name__ == "__main__":
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    if not os.path.exists(app.config['RESULT_FOLDER']):
+        os.makedirs(app.config['RESULT_FOLDER'])
+
+    app.run(debug=True)
+
+import bcrypt
+"""from flask import Flask, render_template, Response, request
+from PIL import Image
+import cv2
+import torch.nn.functional as F  # Add this import
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+import pygame
+from flask import Flask, render_template, url_for, flash, redirect
+from torchvision.models import mobilenet_v2
+from flask_bcrypt import Bcrypt
+from PIL import Image
+from flask_login import UserMixin, login_user, current_user, logout_user, login_required, LoginManager
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from ultralytics import YOLO  # Add this import
 import os
 import psycopg2
 
@@ -32,114 +306,102 @@ def play_alert_sound():
     pygame.mixer.music.play()
 bs = 128
 crop_size = 224
-
 transform = transforms.Compose([
-    transforms.RandomRotation(10),
-    transforms.RandomResizedCrop(crop_size, scale=(0.8, 1.0)),
-    transforms.RandomHorizontalFlip(),
+    transforms.Resize((128, 128)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize for RGB images
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
-
 def stop_alert():
    pygame.mixer.music.stop()  # Assuming you're using pygame.mixer.music for playing the alert sound
 
 
-class CustomMobileNetv2(nn.Module):
-    def __init__(self, output_size):
-        super().__init__()
-        self.mnet = mobilenet_v2(pretrained=True)
-        self.mnet.features[0][0] = nn.Conv2d(3, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-        self.freeze()
-
-        self.mnet.classifier = nn.Sequential(
-            nn.Linear(1280, output_size),
-            nn.Dropout(0.1),
-            nn.LogSoftmax(1)
-        )
+# Define the model
+class CustomModel(nn.Module):
+    def __init__(self, num_classes=3):
+        super(CustomModel, self).__init__()
+        self.mobilenet_v2 = models.mobilenet_v2(pretrained=True)
+        self.mobilenet_v2.classifier = nn.Identity()
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(0.3)
+        self.fc1 = nn.Linear(1280, 8)
+        self.fc2 = nn.Linear(8, num_classes)
 
     def forward(self, x):
-        return self.mnet(x)
+        x = self.mobilenet_v2(x)
+        x = self.flatten(x)
+        x = self.dropout(x)
+        x = torch.relu(self.fc1(x))
+        x = torch.softmax(self.fc2(x), dim=1)
+        return x
 
-    def freeze(self):
-        for param in self.mnet.parameters():
-            param.requires_grad = False
+# Instantiate the model
+model = CustomModel(num_classes=3)
 
-    def unfreeze(self):
-        for param in self.mnet.parameters():
-            param.requires_grad = True
+path = 'b.pth'
 
+model.load_state_dict(torch.load('b.pth'), strict=False)
 
-# Load pre-trained model
-model = CustomMobileNetv2(2)
-path = 'modelmobilenet (2).pt'
-checkpoint = torch.load(path, map_location=torch.device('cpu'))
-
-# Load the state dictionary
-model_state_dict = checkpoint['model_state_dict']
-# Load state dictionary into the model
-model.load_state_dict(model_state_dict)
 model.eval()
+# Set the model to evaluation mode
+
+
 
 # Initialize webcam
 cap = None
 
 # Load Haar Cascade classifier for face detection
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+#face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# Load YOLO model for face detection
+yolo_model = YOLO("yolov8-face/yolov8n-face.pt")
 
 is_detection_started = False
 
 consecutive_drowsy_frames = 0
 alert_triggered = False
 def predict_drowsiness(frame):
-    global consecutive_drowsy_frames, alert_triggered
+    global alert_triggered
     delay_frames = 15  # Set a delay of 30 frames (adjust as needed)
     delay_counter = 0
 
+    # Detect faces in the frame using YOLO
+    results = yolo_model(frame)
 
+    for result in results:
+        for bbox in result.boxes:
+            x1, y1, x2, y2 = map(int, bbox.xyxy[0])
 
-    # Detect faces in the frame
-    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+            # Crop the face
+            face_img = frame[y1:y2, x1:x2]
 
-    for (x, y, w, h) in faces:
-        # Crop the face
-        face_img = frame[y:y + h, x:x + w]
+            # Convert the face image from NumPy array to PIL image
+            face_pil = Image.fromarray(cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB))
 
-        # Convert the face image from NumPy array to PIL image
-        face_pil = Image.fromarray(cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB))
+            # Pre-process the face for model input
+            face_tensor = transform(face_pil).unsqueeze(0)  # Add batch dimension
 
-        # Pre-process the face for model input
-        face_tensor = transform(face_pil).unsqueeze(0)  # Add batch dimension
+            # Make prediction
+            with torch.no_grad():
+                output = model(face_tensor)
+                _, predicted_class = torch.max(output, 1)
 
-        # Make prediction
-        with torch.no_grad():
-            output = model(face_tensor)
-            _, predicted_class = torch.max(output, 1)
+            # Convert prediction to string
+            class_names = ['yawning', 'closeEyes', 'awake']
+            prediction_text = class_names[predicted_class.item()]
 
-        # Convert prediction to string
-        if predicted_class.item() == 0:
-            prediction_text = "Drowsy"
-            consecutive_drowsy_frames += 1
+            if prediction_text in ['closeEyes', 'yawning']:
+                if not alert_triggered:
+                    play_alert_sound()
+                    alert_triggered = True
+            else:
+                if alert_triggered:
+                    stop_alert()
+                    alert_triggered = False
 
-            if consecutive_drowsy_frames >= 10 and not alert_triggered:
-                # play_alert_sound()
-                # alert_triggered = True
-                consecutive_drowsy_frames = 0
+            # Draw rectangle around the face
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-        else:
-            prediction_text = "Not Drowsy"
-            consecutive_drowsy_frames = 0
-
-            if alert_triggered:
-                # Stop the alert if it was triggered
-                # stop_alert()
-                alert_triggered = False  # Reset alert_triggered flag
-
-        # Draw rectangle around the face
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        # Write prediction result on the frame
-        cv2.putText(frame, prediction_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+            # Write prediction result on the frame
+            cv2.putText(frame, prediction_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
 
     return frame
 
@@ -168,7 +430,7 @@ def generate_frames():
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -207,7 +469,7 @@ if __name__ == "__main__":
 
 
 
-    app.run(debug=True)
+    app.run(debug=True)"""
 
 
 
